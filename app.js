526623aad36d614240437baa622cb003e97704cb
wpp-msg-bw <www.wesley.melo@gmail.com>
2025-01-03 21:35:04 -0300

    criado ambiente de esqueceu a senha v3

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.use("/api/auth-admin", authAdminRoutes);//rota do administrador
 
 
 
+
 // Porta do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
